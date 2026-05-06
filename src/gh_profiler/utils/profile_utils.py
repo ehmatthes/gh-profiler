164c787377e927147f3fd6c8df794966625ed269
Eric Matthes <ehmatthes@gmail.com>
2026-05-06 12:59:26 -0400

    Improve performance by making fewer gh calls. (#37)

--- src/gh_profiler/utils/profile_utils.py
+++ src/gh_profiler/utils/profile_utils.py
@@ -4,7 +4,6 @@ import json
 from datetime import datetime as dt
 from datetime import timezone as tz
 from datetime import timedelta
-from urllib.parse import quote
 from textwrap import dedent
 import sys
 
@@ -15,7 +14,8 @@ from . import infra_utils
 def ensure_gh():
     """Make sure user has gh installed and is authenticated.
 
-    DEV: This may need different implementation on Windows or Linux.
+    Check for authentication issues in first external call, rather than making
+    a call just for that purpose here.
     """
     cmd = "gh --version"
     try:
@@ -25,21 +25,20 @@ def ensure_gh():
         msg += "\n  https://cli.github.com"
         sys.exit(msg)
 
-    cmd = "gh api user --jq .login"
-    if not infra_utils.run_cmd(cmd).strip():
-        msg = "The GitHub CLI tool (gh) is not authenticated, or the API hung."
-        msg += "\n  If you've already authenticated, try running the gh-profiler command again."
-        msg += "\n  If you're not authenticated, run `gh auth login`."
-        sys.exit(msg)
-
 
 def get_profile_info():
     """Get all the profile info we'll need."""
     cmd = f"gh api users/{pdata.username} --jq '{{login, name, created_at, company, blog, location, email, bio}}'"
 
     profile_info = infra_utils.run_cmd(cmd)
+    _ensure_authenticated(profile_info)
 
-    pdata.profile_info = json.loads(profile_info)
+    try:
+        pdata.profile_info = json.loads(profile_info)
+    except json.decoder.JSONDecodeError:
+        msg = "Couldn't get GitHub profile info. The gh CLI may have timed out."
+        msg += "\n  You may want to try running the command again."
+        sys.exit(msg)
 
     if "created_at" not in pdata.profile_info:
         sys.exit(f"GitHub user '{pdata.username}' not found.")
@@ -53,23 +52,26 @@ def get_profile_info():
 def get_pr_activity():
     """Get information about recent PR activity."""
     cutoff = (dt.now(tz.utc) - timedelta(days=21)).date().isoformat()
-    base_query = f"author:{pdata.username} is:pr created:>={cutoff}"
 
-    opened_cmd = f'gh api "search/issues?q={quote(base_query)}" --jq .total_count'
-    merged_cmd = (
-        f'gh api "search/issues?q={quote(base_query + " is:merged")}" --jq .total_count'
-    )
-    closed_cmd = f'gh api "search/issues?q={quote(base_query + " is:closed -is:merged")}" --jq .total_count'
+    pr_query = _get_pr_query()
+    search_query = f"author:{pdata.username} is:pull-request created:>={cutoff}"
+    cmd = f"gh api graphql -f query='{pr_query}' -F q='{search_query}' -F n=100"
 
-    # DEV: These calls seem to be timing out occasionally.
     try:
-        pdata.opened_count = int(infra_utils.run_cmd(opened_cmd).strip())
-        pdata.merged_count = int(infra_utils.run_cmd(merged_cmd).strip())
-        pdata.closed_count = int(infra_utils.run_cmd(closed_cmd).strip())
-    except ValueError:
+        data = json.loads(infra_utils.run_cmd(cmd))
+    except json.decoder.JSONDecodeError:
         msg = "Couldn't get recent PR activity. The gh CLI may have timed out."
         msg += "\n  You may want to try running the command again."
         sys.exit(msg)
+
+    search = data["data"]["search"]
+    prs = search["nodes"]
+
+    pdata.opened_count = len(prs)
+    pdata.merged_count = sum(pr["mergedAt"] is not None for pr in prs)
+    pdata.closed_count = sum(
+        pr["state"] == "CLOSED" and pr["mergedAt"] is None for pr in prs
+    )
 
 
 def get_issue_activity():
@@ -85,13 +87,27 @@ def get_issue_activity():
 
     try:
         pdata.issue_activity = json.loads(issue_activity)["data"]["search"]
-    except json.decoder.JSONDecodeError:
+    except (json.decoder.JSONDecodeError, KeyError):
         msg = "Couldn't get recent issue activity. The gh CLI may have timed out."
         msg += "\n  You may want to try running the command again."
         sys.exit(msg)
 
 
 # --- Helper functions ---
+
+
+def _ensure_authenticated(profile_info):
+    """Check that the gh CLI tool has been authenticated.
+
+    This should be called when the first external gh call is made.
+    Making this check on the output of an actual call is more efficent than
+    calling `gh api user --jq .login` just to verify authentication.
+    """
+    if not profile_info.strip():
+        msg = "The GitHub CLI tool (gh) is not authenticated, or the API hung."
+        msg += "\n  If you've already authenticated, try running the gh-profiler command again."
+        msg += "\n  If you're not authenticated, run `gh auth login`."
+        sys.exit(msg)
 
 
 def _get_gh_issues_call(username, cutoff):
@@ -123,3 +139,30 @@ def _get_gh_issues_call(username, cutoff):
     """
 
     return dedent(gh_call).strip()
+
+
+def _get_pr_query():
+    """Return the graphql query for recent PR activity."""
+    pr_query = f"""
+        query($q: String!, $n: Int!) {{
+            search(query: $q, type: ISSUE, first: $n) {{
+                issueCount
+                pageInfo {{
+                hasNextPage
+                endCursor
+                }}
+                nodes {{
+                ... on PullRequest {{
+                    number
+                    state
+                    createdAt
+                    closedAt
+                    mergedAt
+                    url
+                }}
+                }}
+            }}
+            }}
+    """
+
+    return dedent(pr_query).strip()
