37bc343362e383eb160de5453a58a380145a65e1
Eric Matthes <ehmatthes@gmail.com>
2026-05-15 12:31:14 -0700

    Exits if not .git/ dir found.

--- src/gh_profiler/utils/workflow_utils.py
+++ src/gh_profiler/utils/workflow_utils.py
@@ -24,3 +24,10 @@ def _get_workflow_path():
         msg = f"The file {path_pc_workflow.as_posix()} already exists."
         msg += "\nIf you want to generate this file, please delete the existing file and run this command again."
         sys.exit(msg)
+
+    # If there's a .git directory, we can move forward.
+    path_git_dir = path_cwd / ".git"
+    if not path_git_dir.exists() or not path_git_dir.is_dir():
+        msg = f"Could not find a .git dir at: {path_cwd.as_posix()}"
+        msg += "\nAre you in the root directory of your project's repository?"
+        sys.exit(msg)
