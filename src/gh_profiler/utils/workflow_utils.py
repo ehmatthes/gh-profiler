f4abaaf4346613868b5e53ce71241a5e1b4866b0
Eric Matthes <ehmatthes@gmail.com>
2026-05-16 08:31:01 -0700

    Writes .github/workflows directories as needed.

--- src/gh_profiler/utils/workflow_utils.py
+++ src/gh_profiler/utils/workflow_utils.py
@@ -16,7 +16,7 @@ def _get_workflow_path():
     path_workflows = Path.cwd() / ".github" / "workflows"
     path_pc_workflow = path_workflows / "profile_contributors.yml"
 
-    # If the path we want exists and there's no conflict, move forward.
+    # If the workflows directory exists but the workflow file does not, no conflicts.
     if path_workflows.exists() and not path_pc_workflow.exists():
         return path_pc_workflow
 
@@ -26,12 +26,16 @@ def _get_workflow_path():
         msg += "\nIf you want to regenerate this file, please delete the existing file and run this command again."
         sys.exit(msg)
 
-    # If there's a .git directory, we can move forward.
-    path_git_dir = path_cwd / ".git"
+    path_git_dir = Path.cwd() / ".git"
+
+    # If there's no .git directory, we probably shouldn't proceed.
     if not path_git_dir.exists() or not path_git_dir.is_dir():
         msg = f"Could not find a .git dir at: {path_cwd.as_posix()}"
         msg += "\nAre you in the root directory of your project's repository?"
         sys.exit(msg)
+
+    # No conflicts found. Note that .github/workflows/ may not exist.
+    return path_pc_workflow
 
 def _confirm_write_workflow(path_workflow):
     """Confirm the user wants the file written to the calculated location."""
@@ -51,7 +55,15 @@ def _confirm_write_workflow(path_workflow):
 
 def _write_workflow(path_workflow):
     """Write the workflow file to the correct location."""
+    # Read source file.
     path_templates = importlib.resources.files("gh_profiler") / "templates"
     path_src = path_templates / "profile_contributors.yml"
     contents = path_src.read_text()
+
+    # Make .github/workflows dirs as needed.
+    path_workflows = path_workflow.parent
+    if not path_workflows.exists():
+        path_workflows.mkdir(parents=True)
+
+    # Write profile_contributors.yml file.
     path_workflow.write_text(contents)
