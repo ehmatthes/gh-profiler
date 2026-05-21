46bdf6febadf3d78700847ccc7487ea9992acc39
Eric Matthes <ehmatthes@gmail.com>
2026-05-21 09:14:26 -0400

    e2e test for --concise.

--- tests/e2e_tests/test_profiler.py
+++ tests/e2e_tests/test_profiler.py
@@ -5,6 +5,8 @@ Makes an actual gh-profiler call. Currently calls against my own user account
 to use.
 """
 
+import pytest
+
 from gh_profiler.utils import infra_utils
 
 
@@ -21,6 +23,28 @@ def test_full_run():
         "name: Eric Matthes",
         "mastodon: https://fosstodon.org/@ehmatthes",
         "Empty fields: company, bio",
+    )
+
+    for expected_string in expected_strings:
+        assert expected_string in output
+
+def test_concise_run():
+    """Test a --concise run of gh-profiler."""
+    cmd = "uv run gh-profiler ehmatthes --concise"
+    output = infra_utils.run_cmd(cmd)
+
+    if output == "":
+        msg = "Output was empty, which may indicate a gh timeout rather than a problem with the code."
+        pytest.fail(msg)
+
+    # Make assertions about stable parts of output, not entire output string.
+    # DEV: Find some assertions to make about PRs and issues?
+    expected_strings = (
+        "GitHub user: ehmatthes",
+        "🟢 No concerns found with user's profile.",
+        "🟢 No concerns found with recent PR activity.",
+        "🟢 No concerns found with recent issue activity.",
+        "For a more detailed report, run `gh-profiler ehmatthes`.",
     )
 
     for expected_string in expected_strings:
