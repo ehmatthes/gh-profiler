12a2c4fbde611aa48b9ba0067de410521a12f462
Eric Matthes <ehmatthes@gmail.com>
2026-05-23 15:32:50 -0400

    Gets elapsed time for a single run.

---
+++ developer_resources/benchmark.py
@@ -0,0 +1,36 @@
+"""Benchmark real-world performance.
+
+This is meant to help avoid the tool taking longer and longer to run as it
+does more analysis.
+
+Analysis should take no time compared to fetching data. Fetching data should
+not take meaningfully longer than the slowest single fetching call.
+
+This tool helps prove that these claims are holding true.
+
+Goal: Make ~5 runs against a real-world target, rejecting any runs where
+the GH CLI hangs. Report minimum and median time, and full list of times.
+Minimum time is meaningful, max is not. That's more likely to indicate gh
+API response time issues.
+"""
+
+from time import perf_counter
+import shlex
+import subprocess
+
+# from gh_profiler.utils.infra_utils import run_cmd
+
+cmd = "uv run gh-profiler ehmatthes"
+cmd_parts = shlex.split(cmd)
+
+times = []
+while len(times) < 5:
+    ts_start = perf_counter()
+
+    output_obj = subprocess.run(cmd_parts, capture_output=True)
+
+    ts_end = perf_counter()
+    run_time = round((ts_end - ts_start), 2)
+    print(run_time)
+
+    breakpoint()
