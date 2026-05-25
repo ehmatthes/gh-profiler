7e9cc67067e20e8c279b2370840fe57667967f47
Eric Matthes <ehmatthes@gmail.com>
2026-05-25 10:14:59 -0400

    More consistent alignment in output.

--- src/gh_profiler/utils/summary_utils.py
+++ src/gh_profiler/utils/summary_utils.py
@@ -76,7 +76,7 @@ def _get_full_summary():
     # Include profile info.
     summary += _get_profile_header()
     age = humanize.naturaldelta(pdata.account_age)
-    summary += f"  {pdata.flag_age} Account age: {age}\n"
+    summary += f"   {pdata.flag_age} Account age: {age}"
     summary += _profile_summary()
     summary += "\n"
 
@@ -139,15 +139,15 @@ def _username_line():
 def _profile_summary():
     """Summarize information from the user's profile dict."""
     if pdata.flag_profile == flags.red_flag:
-        return f"\n  {pdata.flag_profile} No profile information provided.\n"
+        return f"\n   {pdata.flag_profile} No profile information provided.\n"
 
     # Only show lines for fields that have information.
     # Collect empty fields for last line.
-    summary = f"\n  {pdata.flag_profile} Profile information:\n"
+    summary = f"\n   {pdata.flag_profile} Profile information:\n"
     empty_fields = []
     for k, v in pdata.profile_info.items():
         if v and k != "bio":
-            summary += f"      {k}: {v}\n"
+            summary += f"        {k}: {v}\n"
         elif v and k == "bio":
             summary += _bio_summary(v)
         else:
@@ -155,12 +155,12 @@ def _profile_summary():
 
     # Include socials.
     for social in pdata.socials:
-        summary += f"      {social['provider']}: {social['url']}\n"
+        summary += f"        {social['provider']}: {social['url']}\n"
 
     # List empty fields.
     if empty_fields:
         fields_str = ", ".join(empty_fields)
-        summary += f"     Empty fields: {fields_str}\n"
+        summary += f"      Empty fields: {fields_str}\n"
 
     return summary
 
@@ -183,15 +183,15 @@ def _bio_summary(bio):
 def _pr_activity_summary():
     """Summarize recent PR activity."""
     if pdata.opened_count < 10:
-        return f"  {flags.green_flag} {pdata.username} opened fewer than 10 PRs in the last 21 days.\n"
+        return f"   {flags.green_flag} {pdata.username} opened fewer than 10 PRs in the last 21 days.\n"
 
     summary = ""
     # Only show merged if it's a good sign.
     if pdata.flag_merged_pr == flags.green_flag:
-        summary += f"  {pdata.flag_merged_pr} {pdata.merged_count} of {pdata.opened_count} PRs merged in the last 21 days.\n"
+        summary += f"   {pdata.flag_merged_pr} {pdata.merged_count} of {pdata.opened_count} PRs merged in the last 21 days.\n"
 
     # Include number closed for everyone.
-    summary += f"  {pdata.flag_closed_pr} {pdata.closed_count} of {pdata.opened_count} PRs closed without merging in the last 21 days.\n"
+    summary += f"   {pdata.flag_closed_pr} {pdata.closed_count} of {pdata.opened_count} PRs closed without merging in the last 21 days.\n"
 
     return summary
 
@@ -199,17 +199,17 @@ def _pr_activity_summary():
 def _issue_activity_summary():
     """Summarize recent public issue activity."""
     if pdata.new_issue_count == 0:
-        return f"  {flags.green_flag} {pdata.username} opened no new issues in the last 21 days.\n"
+        return f"   {flags.green_flag} {pdata.username} opened no new issues in the last 21 days.\n"
 
-    summary = f"  {pdata.flag_overall_issues} {pdata.username} opened {pdata.new_issue_count} new issues in the last 21 days.\n"
-    summary += f"     {pdata.flag_issues_not_planned} {pdata.issues_not_planned} issues closed as NOT_PLANNED.\n"
+    summary = f"   {pdata.flag_overall_issues} {pdata.username} opened {pdata.new_issue_count} new issues in the last 21 days.\n"
+    summary += f"      {pdata.flag_issues_not_planned} {pdata.issues_not_planned} issues closed as NOT_PLANNED.\n"
 
     # Repeated issues:
     if pdata.total_repeats == 0:
-        summary += f"     {pdata.flag_repeated_issues} {pdata.total_repeats} issues opened with the same title.\n"
+        summary += f"      {pdata.flag_repeated_issues} {pdata.total_repeats} issues opened with the same title.\n"
     else:
-        summary += f"     {pdata.flag_repeated_issues} {pdata.total_repeats} issues opened with the same title:\n"
+        summary += f"      {pdata.flag_repeated_issues} {pdata.total_repeats} issues opened with the same title:\n"
     for title, count in pdata.repeated_issue_titles.items():
-        summary += f"        {title} ({count})\n"
+        summary += f"         {title} ({count})\n"
 
     return summary
