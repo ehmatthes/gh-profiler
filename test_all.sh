53b9a0a9b7490f1d435c73c0aa6332cc2c5f5788
Eric Matthes <ehmatthes@gmail.com>
2026-05-27 21:32:46 -0400

    test_all.sh includes tests against actual users.

--- test_all.sh
+++ test_all.sh
@@ -4,6 +4,12 @@
 # If they're taking much longer than a few seconds, it's probably worth
 # cancelling the run and run again.
 
+# Unit tests.
 uv run pytest
+
+# E2e tests.
 uv run pytest tests/e2e_tests -k full
 uv run pytest tests/e2e_tests -k concise
+
+# Tests against actual users.
+uv run pytest developer_resources/test_actual_users.py -s
