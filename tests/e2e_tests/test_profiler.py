c0322602726a375f32cda5a3129154be943a52e9
Eric Matthes <ehmatthes@gmail.com>
2026-06-07 11:50:39 -0400

    Draft e2e test for bulk PRs.

--- tests/e2e_tests/test_profiler.py
+++ tests/e2e_tests/test_profiler.py
@@ -128,3 +128,14 @@ def test_redact():
 
     # Should currently see 7 redacted fields in my output.
     assert output.count("<redacted>") == 7
+
+def test_bulk_open_prs():
+    """Test a run against a repo URL for bulk processing open PRs."""
+    # Django is likely to have many open PRs.
+    url = "https://github.com/django/django"
+    cmd = f"uv run gh-profiler {url} -n 3"
+    output = run_with_timeout(cmd)
+
+    assert output.count("PR ") == 3
+    assert output.count("https://github.com/django/django/pull/") == 3
+    assert output.count("GitHub user: ") == 3
